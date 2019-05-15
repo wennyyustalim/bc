e185904f1b457efc811e237ad4303b88c56e9a9d
Wenny Yustalim <wennyyustalim@gmail.com>
2019-05-16 01:41:45 +0700

    Fix script for edge parallel

--- run.sh
+++ run.sh
@@ -5,10 +5,11 @@ rm parallel-cpu.csv
 rm edge.csv
 
 for file in `echo testcases/in/sp500-038 testcases/in/bo testcases/in/cg-web testcases/in/as-rel testcases/in/hep-th`; do
-  for i in `seq 1 5`; do
-    ./bin/edge $file out >> edge.csv;
-    for percent in `echo 1 5 10 20 30 40 50 75 100`; do
+  # for i in `seq 1 5`; do
+    # for percent in `echo 1 5 10 20 30 40 50 75 100`; do
+    for percent in `echo 0 25 50 75 100`; do
+      ./bin/edge $file out $percent >> edge.csv;
       ./bin/parallel-cpu $file out $percent >> parallel-cpu.csv;
     done
-  done
+  # done
 done
