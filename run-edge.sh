24ca17b18da8ed4ad20889ede8911f7ca26a03f3
Wenny Yustalim <wennyyustalim@gmail.com>
2019-05-17 14:35:44 +0700

    Test edge using 1e4 tc for nvprof

--- run-edge.sh
+++ run-edge.sh
@@ -2,7 +2,7 @@
 make edge
 rm edge.csv
 
-for file in `echo testcases/in/flickr`; do
+for file in `echo testcases/in/1e4`; do
   for percent in `echo 0 25 50 75 100`; do
     ./bin/edge $file out $percent >> edge.csv;
   done
