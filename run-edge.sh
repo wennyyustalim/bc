6184e75e0f9333b9c212477ea7fe2062a91be363
Wenny Yustalim <wennyyustalim@gmail.com>
2019-05-17 14:51:22 +0700

    Add nvprof on script

--- run-edge.sh
+++ run-edge.sh
@@ -2,8 +2,8 @@
 make edge
 rm edge.csv
 
-for file in `echo testcases/in/1e4`; do
+for file in `echo testcases/in/1e3`; do
   for percent in `echo 0 25 50 75 100`; do
-    ./bin/edge $file out $percent >> edge.csv;
+    nvprof --output-profile $file.$percent.prof ./bin/edge $file out $percent >> edge.csv;
   done
 done
