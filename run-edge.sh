7ecc5c4d113419dd41da4d4f671f448c25b0c6b4
Wenny Yustalim <wennyyustalim@gmail.com>
2019-05-21 17:14:06 +0700

    Edit run edge script

--- run-edge.sh
+++ run-edge.sh
@@ -2,8 +2,8 @@
 make edge
 rm edge.csv
 
-for file in `echo testcases/in/1e4`; do
+for file in `echo testcases/in/flickr`; do
   for percent in `echo 0 25 50 75 100`; do
-    nvprof --analysis-metrics -o $file.$percent.nvprof ./bin/edge $file out $percent >> edge.csv;
+    ./bin/edge $file out $percent >> edge.csv;
   done
 done
