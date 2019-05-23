09db638e9d0659a3f00d79658a3f6d358d52129a
Wenny Yustalim <wennyyustalim@gmail.com>
2019-05-23 10:54:34 +0700

    Fix profiler script

--- copy-nvprof-results.sh
+++ copy-nvprof-results.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for file in `echo testcases/in/1e4`; do
   for percent in `echo 0 25 50 75 100`; do
-    scp m13515002@167.205.32.100:/home/m13515002/bc/$file.$percent.nvprof result/
+    scp m13515002@167.205.32.100:/home/m13515002/bc/$file.$percent.prof result/
   done
 done
