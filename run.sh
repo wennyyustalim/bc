e4d54e6fffcc0e4bcb4fe442f96851ce367960b3
Wenny Yustalim <wennyyustalim@gmail.com>
2019-05-27 00:19:29 +0700

    Add run epinions

--- run.sh
+++ run.sh
@@ -4,9 +4,8 @@ make edge
 rm parallel-cpu.csv
 rm edge.csv
 
-for file in `echo testcases/in/flickr`; do
+for file in `echo testcases/in/epinions`; do
   # for i in `seq 1 5`; do
-    # for percent in `echo 1 5 10 20 30 40 50 75 100`; do
     for percent in `echo 0 25 50 75 100`; do
       ./bin/edge $file out $percent >> edge.csv;
       ./bin/parallel-cpu $file out $percent >> parallel-cpu.csv;
