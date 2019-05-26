2f744bf43104d34c62b4683e534b7f58117f0b0c
Wenny Yustalim <wennyyustalim@gmail.com>
2019-05-26 23:30:33 +0700

    Add test run

--- test.sh
+++ test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-nvcc cpugpu.cu -o hybrid -std=c++11 -O2 -Xcompiler -fopenmp
-nvcc parallel-edge.cu -o edge -std=c++11 -O2
+nvcc cpugpu.cu -o bin/cpugpu -std=c++11 -O2 -Xcompiler -fopenmp
+nvcc parallel-edge.cu -o bin/edge -std=c++11 -O2
 
 for file in `echo custom1 custom2`; do
-  ./hybrid testcases/in/$file out 5 > testcases/out/$file.hybrid;
-  ./edge testcases/in/$file out > testcases/out/$file.edge;
-  diff -q testcases/out/$file.hybrid testcases/ans/$file;
+  ./bin/cpugpu testcases/in/$file out 5 > testcases/out/$file.cpugpu;
+  ./bin/edge testcases/in/$file out > testcases/out/$file.edge;
+  diff -q testcases/out/$file.cpugpu testcases/ans/$file;
   diff -q testcases/out/$file.edge testcases/ans/$file;
 done
